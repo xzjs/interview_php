dfe8758a7d5fc8446ddab9bdc42399a2c0a4390a
血之君殇 <2310976219@qq.com>
2014-11-29 16:24:44 +0800

    谢了好多，这儿都不知道改写什么了

--- t31.php
+++ t31.php
@@ -59,10 +59,10 @@
 				$second->next=null;
 				// $second->next=$new2;
 			}
-			echo "dummy";
-			var_dump($dummy);
-			echo "pivot";
-			var_dump($pivot);
+			// echo "dummy";
+			// var_dump($dummy);
+			// echo "pivot";
+			// var_dump($pivot);
 			$curr=$next;
 		}
 		$first->next=$pivot->next;
@@ -86,4 +86,39 @@
 	printList($L);
 	printList(partitionLinkedList($L,3));
 
+?>
+<h1>链表去重</h1>
+<h2>输入一个有序链表，删除重复值，确保每个值只出现一次</h2>
+<?php
+	function deleteDuplicates($head)
+	{
+		if($head==null){
+			return $head;
+		}
+		$prev=$head;
+		$curr=$head->next;
+		while ($curr!=null) {
+			if($curr->val!=$prev->val){
+				$prev=$prev->next;
+			}
+			$curr=$curr->next;
+			$prev->next=$curr;
+		}
+		return $head;
+	}
+
+	$arr=array(1,1,1,2,2,3);
+	$L=new ListNode(0);
+	$L1=$L;
+	foreach ($arr as $nodeval) {
+		// echo $nodeval." ";
+		$new = new ListNode($nodeval);
+		// echo $new->val.' ';
+		$L1->next=$new;
+		$L1=$new;
+		// echo $L1->val;
+		// echo "<br/>";
+	}
+	printList($L);
+	printList(deleteDuplicates($L));
 ?>
